1cf7eec29ffebf724dd05f9bc5a894d88c77f89e
Nathan Schubert <schubert.nathan@gmail.com>
2018-07-03 17:46:37 -0400

    able to render header component

---
+++ clicky-game/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game!</h1>
+    <h2>
+      See how many times you can click on a different without repeats though!
+    </h2>
+  </header>
+);
+
+export default Header;
